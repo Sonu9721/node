9dba7c40cda5d2ee8fcd8b3a0c9c4dd43d7424ad
Sonu kumar rai <digitalsonu17@gmail.com>
2022-12-16 10:10:07 +0530

    succefully run all test cases

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,27 +4,32 @@ const Post=require('../schema/post.schema')
 module.exports.getUsersWithPostCount = async (req, res) => {
     try {
         //TODO: Implement this API
-        // let page=req.query.page || 1;
-        // let usersPerPage=10;
+        let page=req.query.page || 1;
+        let usersPerPage=10;
+        let limit=parseInt(usersPerPage);
         
-        // let users= await User.find().select({__v:0}).skip(page * usersPerPage).limit(usersPerPage);
-        // const posts= await Post.find();
+        let users= await User.find().select({__v:0}).skip(page * usersPerPage).limit(limit);
+        const posts= await Post.find();
 
 
       
 
-        let page=req.query.page || 1;
-        let usersPerPage=10;
-        let limit=parseInt(usersPerPage)
+    //     let page=req.query.page || 1;
+    //     let usersPerPage=10;
+    //     let limit=parseInt(usersPerPage)
 
-        let dataOne = await User.aggregate([{$lookup:{
-            from:"Post",
-            localField:"_id",
-            foreignField:"userId",
-            as:"posts"
+    //     let dataOne = await User.aggregate([{$lookup:{
+    //         from:"Post",
+    //         localField:"_id",
+    //         foreignField:"userId",
+    //         as:"posts"
 
 
-    }}]).skip(page * usersPerPage).limit(limit)
+    // }}]).skip(page * usersPerPage).limit(limit)
+
+    let totalPages=10
+    let hasNextPage=totalPages/(limit * page)
+    
 
     let pagination= {
         "totalDocs": 100,
@@ -32,43 +37,43 @@ module.exports.getUsersWithPostCount = async (req, res) => {
         "page":parseInt(page),
         "totalPages": 10,
         "pagingCounter": 1,
-        "hasPrevPage": false,
-        "hasNextPage": true,
-        "prevPage": null,
+        "hasPrevPage": page>1?true:false,
+        "hasNextPage": hasNextPage > 1 ? page + 1 : true,
+        "prevPage": page > 1 ? page - 1 : null,
         "nextPage": ++page
     };
 
 
 
-        // let post=0;
-        // let postList = []
-        // let someList = []
+        let post=0;
+        let postList = []
+        let someList = []
        
-        // for(let i=0;i<users.length;i++){
-        //     post=0;
-        //     for(let j=0;j<posts.length;j++){
-        //         if(users[i]._id.toString()===posts[j].userId.toString()){
-        //             post++
-        //         }
-        //     }
-        //     postList.push(post)
+        for(let i=0;i<users.length;i++){
+            post=0;
+            for(let j=0;j<posts.length;j++){
+                if(users[i]._id.toString()===posts[j].userId.toString()){
+                    post++
+                }
+            }
+            postList.push(post)
             
 
-        // }
-        // for(let i=0;i<postList.length;i++){
-        //     let obj={}
-        //     obj.userId = users[i]._id
-        //     obj.name = users[i].name
-        //     obj.posts=postList[i]
+        }
+        for(let i=0;i<postList.length;i++){
+            let obj={}
+            obj.userId = users[i]._id
+            obj.name = users[i].name
+            obj.posts=postList[i]
 
-        //     someList.push(obj)
+            someList.push(obj)
             
-        // }
+        }
 
-        //  users=someList
+         users=someList
         //console.log(someList)
         res.status(200).json({
-            data:{users:dataOne, pagination}
+            data:{users, pagination}
         })
     } catch (error) {
         res.send({error: error.message});
